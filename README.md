44dbe7fb4ae7bc48a9447a362204e55d070345e3
cammy.tang <cammy.tang@endeavorstreaming.com>
2022-11-02 11:20:56 +0800

    feat: Add assets-content-hash-plugin

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # assets-content-hash-plugin
+
+This Webpack plugin will
+
+1. generate contenthash for every file you pass by param 'file';
+2. rename filename to new one which include hash, format xxx.yyy.[hash].[js|css]
+3. replace file with new filename in index.html
+
+***TODO: Auto inject assets file to index.html with correct meta tag.***
+
+## Why Is This Useful?
+
+You may use some static files in index.html, which are assets under public path.
+
+When these static files changed, user may can't get the updated ones because of browser cache. Containing content hash in filename can avoid the cache issue.
+
+
+
+### Example output:
+
+The output is a file whose name contains its content hash:
+
+```html
+my-app/build/config.510e9f86.js
+```
+
+sync filename in build/index.html
+
+```html
+<script src='https://myapp.com/config.510e9f86.js'></script>
+```
+
+## Install
+
+```sh
+npm install assets-content-hash-plugin
+```
+
+## Configuration
+
+In your webpack config include the plug-in. And add it to your config:
+
+```js
+import AssetsContentHashPlugin from 'assets-content-hash-plugin';
+// ...
+module.exports = {
+    // ....
+    plugins: [new AssetsContentHashPlugin({
+      file:['config.js','common.css']
+    })]
+}
+```
+
+### Options
+
+You can pass the following options:
+
+#### `file`
+Required. string or string array. Asset file path.
+
+#### `html`
+
+Optional. `index.html` by default.
---
+++ index.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+/*
+input param
+{
+  file: string|array; // filepath, like 'config.js'
+  hasInjectToHtml?: boolean; // default to be true. if false, plugin can inject the files to html.
+  html?: string; // default to be 'index.html'
+}
+features:
+1.generate a file include build hash;
+2.rename filename to new one which include hash, format xxx.yyy.[hash].[js|css]
+3.replace file with new filename in index.html
+ */
+class BuildHashPlugin {
+	constructor(options) {
+		this.options = {
+			file: [],
+			hasInjectToHtml: true,
+			html: 'index.html',
+			hashNumber: 8,
+		};
+
+		if (!options) {
+			return;
+		}
+
+		if (options.file) {
+			if (typeof options.file === 'string') {
+				this.options.file.push(options.file);
+			} else if (Object.prototype.toString.call(options.file) === '[object Array]') {
+				this.options.file = options.file;
+			} else {
+				const err = new Error('file is required, should be string or array!');
+				throw err;
+			}
+		} else {
+			const err = new Error('file is required, should be string or array!');
+			throw err;
+		}
+
+		if (options.hasInjectToHtml) {
+			this.options.hasInjectToHtml = options.hasInjectToHtml;
+		}
+
+		if (options.html) {
+			this.options.html = options.html;
+		}
+	}
+
+	analyzeFile(file) {
+		const fileArry = file.split('/');
+		let filename = file,
+				filepath = file;
+		if (fileArry.length >= 1) {
+			filename = fileArry[fileArry.length - 1];
+		}
+		return {
+			filename,
+			filepath,
+		};
+	}
+
+	generateNewFilename(filename, hash) {
+		const file = filename.split('.');
+		if (file.length < 2) {
+			return filename;
+		} else {
+			const filetype = file[file.length - 1];
+			file.fill(hash, file.length - 1).push(filetype);
+			return file.join('.');
+		}
+	}
+
+	apply(compiler) {
+		const indexFile = path.resolve(compiler.options.output.path || '.', this.options.html); // index.html
+
+		compiler.hooks.afterEmit.tap('BuildHashPlugin', (compilation, callback) => {
+			const that = this;
+
+			(this.options.file || []).forEach((fileItem) => {
+				const file = that.analyzeFile(fileItem);
+				const filename = file.filename;
+				const filepath = file.filepath;
+				const staticFile = path.resolve(compiler.options.output.path || '.', filepath);
+
+				const buffer = compiler.outputFileSystem.readFileSync(staticFile, 'utf8');
+				const contentHash = loaderUtils.getHashDigest(buffer);
+
+				const newFilename = this.generateNewFilename(filename, contentHash);
+				// Step 1: replace filename in index.html
+				if (this.options.hasInjectToHtml) {
+					let indexHtml = compiler.outputFileSystem.readFileSync(indexFile, 'utf8');
+					indexHtml = indexHtml.replace(filename, newFilename); //TODO use filepath to check
+					compiler.outputFileSystem.writeFileSync(indexFile, indexHtml, callback);
+				}
+				// Step 2: rename filename
+				const newFile = staticFile.replace(filename, newFilename);
+				compiler.outputFileSystem.renameSync(staticFile, newFile);
+				if (!this.options.hasInjectToHtml) {
+					//TODO: need inject the file to html
+				}
+			});
+		});
+	}
+}
+
+module.exports = BuildHashPlugin;
